88e29b9af6ab55ca33b82b504156c795d217e072
Shubhansu-kumar <61730102+Shubhansu-kumar@users.noreply.github.com>
2022-09-25 20:44:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,6 @@ import Apple from "./News/Apple"
 
 export default function App() 
 {
-  // let [value, setValue] = useState("");
-
-  // let value
-  // async function data(){
-  //   let resp=await fetch("https://newsapi.org/v2/everything?q=bitcoin&apiKey=e5c579761d6e46c98fc217bcec91924a");
-  //   let rest= await resp.json();
-  //   console.log(rest);
-  //   console.log(rest.articles[0].content);
-  // }
-  // data();
   return <div> <Bitcoin/>
   <Apple/>
    </div>;
