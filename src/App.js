6eff0b305a80e9eb45929e13b1a31c7b40bc822d
Shubahsnu <kumarshubahsnu@gmail.com>
2022-09-25 20:24:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Bitcoin from "./News/Bitcoin"
+import Apple from "./News/Apple"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() 
+{
+  // let [value, setValue] = useState("");
+
+  // let value
+  // async function data(){
+  //   let resp=await fetch("https://newsapi.org/v2/everything?q=bitcoin&apiKey=e5c579761d6e46c98fc217bcec91924a");
+  //   let rest= await resp.json();
+  //   console.log(rest);
+  //   console.log(rest.articles[0].content);
+  // }
+  // data();
+  return <div> <Bitcoin/>
+  <Apple/>
+   </div>;
 }
-
-export default App;
---
+++ src/News/Bitcoin.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+export default function Bitcoin() {
+  let [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://newsapi.org/v2/everything?q=bitcoin&apiKey=f6ce4b5880c6484997b23d998a3fda5b`
+    )
+      .then((rest) => rest.json())
+      .then((rest) => setData(rest.articles));
+  });
+
+  return (
+          <div className="p-10 bg-[#e7ead3] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5">
+            {data.map((items) => {
+              return (
+                <div className="rounded bg-[#e7e1e2] overflow-hidden shadow-lg">
+                  <img
+                    className="w-full"
+                    src={items.urlToImage}
+                    alt="bitcoin"
+                  />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{items.title}</div>
+                    <p className="text-gray-700 text-base">
+                      {items.description}
+                      <button className=" text-red-600 ">
+                        <a href={items.url}>........Read More</a>
+                      </button>
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+  );
+}
